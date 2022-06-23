cae0e4fd89c5e92caa59f81eab213702bb677aba
Ali ihsan Cengiz <34631659+aliihsancengiz@users.noreply.github.com>
2022-06-23 13:21:47 +0300

    Make EventBus singleton (#17)
    Co-authored-by: ihsan.cengiz <ihsan.cengiz@bosphorusiss.com>

--- EventBus/EventBus.hpp
+++ EventBus/EventBus.hpp
@@ -12,14 +12,38 @@ namespace event_bus
     {
         using eventHandler = std::function<void(std::any)>;
 
+        static EventBus &getInstance()
+        {
+            static EventBus bus;
+            return bus;
+        }
+
         template <typename Event, typename Handler>
-        void addHandler(Handler handler);
+        void addHandler(Handler handler)
+        {
+            const auto type_idx = std::type_index(typeid(Event));
+            handlerRegister.emplace(type_idx, [func = std::forward<Handler>(handler)](auto value)
+                                    { func(std::any_cast<Event>(value)); });
+        }
         template <typename Event>
-        void removeEvent();
+        void removeEvent()
+        {
+            handlerRegister.erase(std::type_index(typeid(Event)));
+        }
         template <typename Event>
-        void fireEvent(Event ev);
+        void fireEvent(Event ev)
+        {
+            auto [beginEventIter, endEventIter] =
+                handlerRegister.equal_range(std::type_index(typeid(Event)));
+            for (; beginEventIter != endEventIter; ++beginEventIter)
+            {
+                beginEventIter->second(std::forward<Event>(ev));
+            }
+        }
 
     private:
+        EventBus() {}
         std::unordered_multimap<std::type_index, eventHandler> handlerRegister;
     };
+
 } // namespace event_bus
